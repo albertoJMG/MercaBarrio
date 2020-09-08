065740681e57a6e0f49efb568342ad7d942fceeb
albertoJMG <albertojmg@gmail.com>
2020-09-08 22:22:27 +0200

    Terminado el tema del stock

--- build/generated-sources/ap-source-output/entidades/Cliente_.java
+++ build/generated-sources/ap-source-output/entidades/Cliente_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-08T21:19:14")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-08T22:16:42")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ extends Usuario_ {
 
