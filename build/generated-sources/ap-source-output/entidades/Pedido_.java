ba09310d26a41064ac2128353635fa4603b3015b
albertoJMG <albertojmg@gmail.com>
2020-09-27 14:17:51 +0200

    Depuracion Inicial y Responsive. solo en xhtml

--- build/generated-sources/ap-source-output/entidades/Pedido_.java
+++ build/generated-sources/ap-source-output/entidades/Pedido_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-26T12:46:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-27T14:10:00")
 @StaticMetamodel(Pedido.class)
 public class Pedido_ { 
 
