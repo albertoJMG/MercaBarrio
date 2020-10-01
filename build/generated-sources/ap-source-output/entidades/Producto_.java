ca4c346c2ca81c16465977f2d851ffb2708f07a6
albertoJMG <albertojmg@gmail.com>
2020-10-01 17:06:40 +0200

    Responsive terminado. Falta panelgrid de los productos

--- build/generated-sources/ap-source-output/entidades/Producto_.java
+++ build/generated-sources/ap-source-output/entidades/Producto_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-09-29T21:21:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-10-01T17:05:32")
 @StaticMetamodel(Producto.class)
 public class Producto_ { 
 
